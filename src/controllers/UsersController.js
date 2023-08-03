8400017f7b31cc8b0b84ee2b76779062af38dc19
Daiane Araujo <daiane.ajmachado@gmail.com>
2023-08-03 16:21:55 -0300

    feat: add create user controller

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,27 @@
+const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
+const { hash } = require("bcryptjs");
 
 class UsersController {
-  create(request, response) {
-    const { name, email, password } = request.body;
+  async create(request, response) {
+    const { name, email, password, is_admin } = request.body;
 
-    if (!name) {
-      throw new AppError("Nome é obrigatório");
-    }
+    const userAlreadyExists = await knex("users").where({ email }).first();
 
-    response.status(201).json({ name, email, password });
+    if (userAlreadyExists) {
+      throw new AppError("O e-mail informado já está em uso.")
+    };
+
+    const hashedPassword = await hash(password, 8);
+
+    await knex("users").insert({
+      name,
+      email,
+      password: hashedPassword,
+      is_admin
+    })
+
+    response.status(201).json();
   };
 };
 
